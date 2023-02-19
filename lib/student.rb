d9077b8c8a0d36a55a87994c5c27f21619921c43
oprah <oprah.chemirmir@student.moringaschool.com>
2023-02-19 22:59:14 +0300

    done with assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
 
+
+require_relative './user'
+
+class Student < User
+ def initialize 
+    @knowledge = []
+ end
+ def learn vale
+    @knowledge << vale
+
+ end
+ def knowledge
+    @knowledge
+ end
 end
